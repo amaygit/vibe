2b5dde6fded95c046a54b19b83010afe2abf2d6b
amaygit <mailamaynayak@gmail.com>
2025-07-01 16:10:38 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button'
 import React from 'react'
 
-const page = () => {
+const page =() => {
   return (
     <div>
-   <Button> Click me</Button>
+      hello world
    </div>
   )
 }
